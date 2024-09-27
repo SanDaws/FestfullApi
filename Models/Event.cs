cefe2ce3b87dd61279d5b9b3553636b109bb4d6a
sanDaws <castrogiraldosantiago@gmail.com>
2024-09-27 12:18:30 -0500

    FEat: models

---
+++ Models/Event.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FestfullApi.Models
+{
+    public class Event
+    {
+        
+    }
+}
