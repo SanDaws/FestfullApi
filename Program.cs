1f3b4e94ef5f8fd400aac55152d9341c3b55c38a
sanDaws <castrogiraldosantiago@gmail.com>
2024-09-27 12:18:15 -0500

    Feat: dbConection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using DotNetEnv;
+using FestfullApi.data;
+using Microsoft.EntityFrameworkCore;
 
 namespace FestfullApi;
 
@@ -6,6 +9,16 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        Env.Load();
+        var FestFullModuleConection = $"server={Environment.GetEnvironmentVariable("HOST")};" +
+                                   $"port={Environment.GetEnvironmentVariable("PORT")};" +
+                                   $"database={Environment.GetEnvironmentVariable("DATABASE")};" +
+                                   $"uid={Environment.GetEnvironmentVariable("USER")};" +
+                                   $"password={Environment.GetEnvironmentVariable("PASSWORD")}";
+        builder.Services.AddDbContext<FestFullApiDbContext>(options =>options.UseNpgsql(FestFullModuleConection));
+            
+
 
         // Add services to the container.
 
