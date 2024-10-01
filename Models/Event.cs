e457d1b9af5e35775de674ecb06598bff04c93f9
sandaws <castrogiraldosantiago@gmail.com>
2024-10-01 12:53:09 -0500

    Fix: event key

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FestfullApi.Models;
 public class Event
 {
-    public required string eventName{get;set;}
+    [Key]
+    [Column("event_id")]
+    public uint EventId{get;set;}
+    [Column("event_name")]
+    public required string EventName{get;set;}
+    [Column("tables")]
     public required uint Tables{get;set;}
-    public required uint CahrPerTable{get;set;}
+    [Column("chairs_per_table")]
+    public required uint ChairPerTable{get;set;}
+    [Column("event_Date")]
     public required DateTime EventDate{get;set;}
+    [Timestamp]
+    [Column("Event_stamp")]
     public DateOnly CreationEventDate{get;set;}
     
 
