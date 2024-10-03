ea878f60a5a3d7addb62bb0142713c993c91205d
sanDaws <castrogiraldosantiago@gmail.com>
2024-10-03 10:20:35 -0500

    Feat: controllers repositories and services files

---
+++ Controllers/Guest/GuestUpdateController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FestfullApi.Controllers.Guest
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestUpdateController : ControllerBase
+    {
+        
+    }
+}
