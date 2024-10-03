212b4eb2302ed83770f021a5019f05f31f4c300d
sanDaws <castrogiraldosantiago@gmail.com>
2024-10-03 10:21:47 -0500

    FEat:format repositories

--- Repositories/ICompanionRepository.cs
+++ Repositories/ICompanionRepository.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FestfullApi.Repositories
-{
+namespace FestfullApi.Repositories;
+
     public interface ICompanionRepository
     {
         
     }
-}
--- Repositories/IEventRepository.cs
+++ Repositories/IEventRepository.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace FestfullApi.Repositories
-{
+namespace FestfullApi.Repositories;
     public interface IEventRepository
     {
-        
+     
     }
-}
