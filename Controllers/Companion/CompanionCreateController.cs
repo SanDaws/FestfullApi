31e39893b8cf54eb089f31383f61e8df6b0becc2
Daws <castrogiraldosantiago@gmail.com>
2024-10-04 08:08:12 -0500

    Feat: JWT implementation

--- Controllers/Companion/CompanionCreateController.cs
+++ Controllers/Companion/CompanionCreateController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FestfullApi.Repositories;
 using FestfullApi.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FestfullApi.Controllers.Companion
 {
@@ -12,6 +13,7 @@ namespace FestfullApi.Controllers.Companion
     [Route("api/[controller]")]
     public class CompanionCreateController(ICompanionRepository companionRepository) : CompanionConectionController(companionRepository)
     {
+        //[Authorize]// for  make it required a jwt
         [HttpGet]
         public Task<ActionResult<IEnumerable<Companion>>> getAll(){
             return Ok("sabreoso");
